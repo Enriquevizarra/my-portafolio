a13a73ba459ed61cd817e76f815b739001b9b572
Enriquevizarra <vizarramattay@gmail.com>
2023-03-03 11:38:08 -0500

    ADD: create folder footer and component Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import "./footer.css";
+import "../../App.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Vizarra</h1>
+
+        <div className="footer__social">
+          <a
+            href="https://www.instagram.com/enriquev.50/"
+            className="footer__social-link"
+            // target="_blank"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.facebook.com/enriqueyeferson.vizarramatta/"
+            className="footer__social-link"
+            // target="_blank"
+          >
+            <i className="bx bxl-facebook"></i>
+          </a>
+
+        </div>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portafolio" className="footer__link">
+              projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+        
+        <span
+          className="footer__copy"
+        >
+          &#169; Enrique-Vizarra   
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export { Footer };
