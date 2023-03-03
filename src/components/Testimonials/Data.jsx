a594b5bd66fdb156fc5479dde793b63a9ecfb164
Enriquevizarra <vizarramattay@gmail.com>
2023-03-03 16:11:04 -0500

    ADD: create component Data.jsx

---
+++ src/components/Testimonials/Data.jsx
@@ -0,0 +1,27 @@
+import image1 from "../../assets/testimonial1.png"
+import image2 from "../../assets/testimonial2.png"
+import image3 from "../../assets/testimonial3.png"
+
+
+const Data = [
+  {
+    id:1,
+    image: image1,
+    title: "Joe Doe",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident ab necessitatibus placeat velit quas. Vel, eveniet blanditiis? Sed odio consectetur veritatis suscipit praesentium minus, vel possimus nesciunt eos dolores aspernatur!"
+  },
+  {
+    id:2,
+    image: image2,
+    title: "Mariana Clinton",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident ab necessitatibus placeat velit quas. Vel, eveniet blanditiis? Sed odio consectetur veritatis suscipit praesentium minus, vel possimus nesciunt eos dolores aspernatur!"
+  },
+  {
+    id:3,
+    image: image3,
+    title: "Sara cill",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident ab necessitatibus placeat velit quas. Vel, eveniet blanditiis? Sed odio consectetur veritatis suscipit praesentium minus, vel possimus nesciunt eos dolores aspernatur!"
+  },
+]
+
+export { Data }
