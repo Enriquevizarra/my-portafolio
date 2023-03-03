23283ce77955dca0589296115648f6c70e3f169a
Enriquevizarra <vizarramattay@gmail.com>
2023-03-03 16:11:52 -0500

    ADD: components in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { About } from './components/about/About';
+import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
+import { Home } from './components/home/Home';
+import { Qualification } from './components/qualification/Qualification';
+import { ScrollUp } from './components/scrollup/ScrollUp';
+import { Services } from './components/services/Services';
+import { Skills } from './components/skills/Skills';
+import { Testimonials } from './components/Testimonials/Testimonials';
 
 function App() {
   return (
     <div>
-      hello
+      <Header />
+      
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Testimonials />
+        <Contact />
+        
+      </main>
+        <Footer />
+        <ScrollUp />
     </div>
   );
 }
